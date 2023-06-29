ccd907f6face81da67e6f574b0ac177d157da4e9
poncy <poncyman@gmail.com>
2023-06-29 16:01:38 +0200

    Add useContext -own example

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,19 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
+import CartProvider from "../../store/CartProvider";
+import { useContext } from "react";
 
 const HeaderCartButton = props => {
+	const cc = useContext(CartContext);
+
 	return (
 		<button className={classes.button} onClick={props.onShowCart}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
 			<span>Your Card</span>
-			<span className={classes.badge}>3</span>
+			<span className={classes.badge}>{cc.items.length}</span>
 		</button>
 	);
 };
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,26 @@
 import Input from "../../UI/Input";
 import CartContext from "../../../store/cart-context";
+import { useState, useContext } from "react";
 import classes from "./MealItemForm.module.css";
+
 const MealItemForm = props => {
+	const [amountMeals, setAmountMeals] = useState(1);
+	const cc = useContext(CartContext);
+	const changeAmountHandler = event => {
+		setAmountMeals(event.target.value);
+	};
+
 	const onSubmit = event => {
 		event.preventDefault();
+		const item = {
+			id: props.id,
+			name: props.name,
+			price: props.price * amountMeals,
+		};
+		cc.addItem(item);
 	};
 	return (
-		<form className={classes.form}>
+		<form className={classes.form} onSubmit={onSubmit}>
 			<Input
 				label='Amount'
 				input={{
@@ -17,6 +31,7 @@ const MealItemForm = props => {
 					step: "1",
 					defaultValue: "1",
 				}}
+				onChange={changeAmountHandler}
 			/>
 			<button>+ Add</button>
 		</form>
